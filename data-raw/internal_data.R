8286cc1146bb87c097a45459820af479549904d7
Motoki Saito <motokisaito.8623@gmail.com>
2023-10-04 19:25:50 +0200

    Third submission to CRAN
    On branch master
    Changes to be committed:
          modified:   CRAN-SUBMISSION
          modified:   R/sysdata.rda
          modified:   cran-comments.md
          modified:   data-raw/internal_data.R
          modified:   tests/testthat/helper-config.R

--- data-raw/internal_data.R
+++ data-raw/internal_data.R
@@ -1,21 +1,5 @@
 library(mgcv)
 
-### For testing ###
-set.seed(534)
-invisible(capture.output(tdat <- gamSim(eg=6,verbose=FALSE)))
-
-tmdl0  <- gam(y ~ s(x0, by=fac) + s(x1, by=fac, k=5) + s(x2)
-	        + ti(x0,x1,by=fac) + ti(x0,x2,by=fac), data=tdat)
-x2max <- max(tmdl0$model$x2)
-
-tmdl1  <- gam(y ~ s(x0), data=tdat)
-
-tmdl2 <- gam(y ~ s(x0, by=fac) + s(x1, by=fac, k=5) + ti(x0,x1), data=tdat)
-tdat3 <- tdat
-set.seed(432)
-tdat3$foo <- factor(sample(LETTERS[1:3], nrow(tdat3), replace=TRUE))
-tmdl3 <- gam(y ~ s(x0, by=fac) + s(x1, by=foo) + ti(x0,x1, by=fac), data=tdat3)
-
 ### For README and vignettes ###
 example_df <- function (n=1000) {
 	set.seed(123)
@@ -56,5 +40,4 @@ mdl_fac <- gam(y ~ s(x1, by=fac) + s(x2, by=fac)
 
 
 ### Output ###
-usethis::use_data(tdat, tdat3, tmdl0, tmdl1, tmdl2, tmdl3, x2max, mdl,
-		  mdl_fac, internal=TRUE, overwrite=TRUE)
+usethis::use_data(mdl, mdl_fac, internal=TRUE, overwrite=TRUE)
